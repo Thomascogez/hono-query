c09e20619a3ad776663856c00c2303743152e6b1
Thomas Cogez <thomas.cogez@gmail.com>
2025-09-19 11:17:47 +0200

    chore: disable tsdown dts generation for react package

--- packages/react/tsdown.config.ts
+++ packages/react/tsdown.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "tsdown/config";
 
 export default defineConfig({
 	entry: "src/index.ts",
-	dts: true,
 	minify: true,
 	external: ["@tanstack/react-query", "hono"]
 });
