0fa14963364f4e9581290bfecf6214b9f0879120
Thomas Cogez <thomas.cogez@gmail.com>
2025-09-19 10:16:23 +0200

    fix(build): dts bundling and external package

--- packages/react/tsdown.config.ts
+++ packages/react/tsdown.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from "tsdown/config";
 
 export default defineConfig({
 	entry: "src/index.ts",
-	dts: true
+	dts: true,
+	minify: true,
+	external: ["@tanstack/react-query", "hono"]
 });
